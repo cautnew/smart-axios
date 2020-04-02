af9a0bc9f31e5533805636092d19047385f76e0f
cl4pper <cl4pper@gmail.com>
2020-04-02 00:42:56 -0300

    refact: create default return from GET response

--- src/module/get.js
+++ src/module/get.js
@@ -1,10 +1,20 @@
 const axios = require('axios');
 
+const defaultResponse = ({ status, data = null, error = [] }) => ({
+	status,
+	data,
+	error
+});
+
 function axiosGet(path, success, failure) {
 	return axios
 		.get(path)
-		.then(data => success(data))
-		.catch(err => failure(err));
+		.then(data => {
+			return success(defaultResponse({ ...data }));
+		})
+		.catch(err => {
+			return failure(defaultResponse({ ...err.response, error: err.response.statusText }));
+		});
 }
 
 exports.axiosGet = axiosGet;
